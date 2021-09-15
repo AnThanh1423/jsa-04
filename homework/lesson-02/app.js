555f78534abc51c1f475da94ce5fb3435a8fbe35
AnThanh1423 <thanhan1423@gmail.com>
2021-09-15 20:12:52 +0700

    NewPush

--- homework/lesson-02/app.js
+++ homework/lesson-02/app.js
@@ -1,1 +1,62 @@
 //lesson-02
+// HW
+// 1. Return the Remainder from Two Numbers
+// function remainder(a, b) {
+//         let rem = (a % b)
+//         return rem;
+//     }
+//     console.log(remainder(3, 4));
+
+//2. Find the Perimeter of a Rectangle
+// function findPerimeter(l, w) {
+//         let perimeter = (l + w) * 2
+//         return perimeter;
+//     }
+//     console.log(findPerimeter(2, 9));
+
+//3. Basketball Points
+// function BasketPoints(n, s) {
+//         let points = n * 2 + s * 3
+//         return points;
+//     }
+//     console.log(BasketPoints(7, 5));
+//4. The Farm Problem
+// function FarmPro(c, o, p) {
+//         let legs = c * 2 + o * 4 + p * 4
+//         return legs;
+//     }
+//     console.log(FarmPro(1, 2, 3));
+//5. Football Points
+// function FBPoints(w, d, l) {
+//         let TotalP = w * 3 + d * 1 + l * 0
+//         return TotalP;
+//     }
+//     console.log(FBPoints(0, 0, 1));
+//6. Sum of Polygon Angles
+// function Sides(n) {
+//         if (n < 2)
+//         return 0;
+//         return (n - 2) * 180;
+//     }
+//     console.log(Sides(6));
+//7. Let's Fuel Up!
+// function Distance(d) {
+//         let f = d * 10
+//         if (f >= 100)
+//         return f;
+//         return 100;
+//     }
+//     console.log(Distance(3));
+//8. Moving House
+// function MovingHouse(a, m) {
+//         let yearsInOneHouse = a / (m + 1)
+//         return yearsInOneHouse;
+//     }
+//     console.log(MovingHouse(15, 2));
+//9. Frames Per Second
+// function FramesPerSecond(f, m) {
+//         let FPS = f * m * 60
+//         return FPS;
+//     }
+//     console.log(FramesPerSecond(10, 1));
+//Done HW
